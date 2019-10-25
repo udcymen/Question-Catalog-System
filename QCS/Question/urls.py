a9b9b76044d554971a6db578c37d52860ad450d2
Cong Meng <samuelmc1015@gmail.com>
2019-10-25 16:56:36 -0400

    rebuild api

--- QCS/Question/urls.py
+++ QCS/Question/urls.py
@@ -4,12 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.detail, name='detail'),
+    path('<int:question_id>', views.detail, name='detail'),
     path('index', views.home, name='home'),
-    # path('', views.index, name='index'),
-    # path('index', views.question_index, name='question_index'),
-    # # path('question/', views.index, name='index'),
-    # # path('question/<str:id>', views.detail, name='detail'),
-    # path('question/<int:question_id>', views.get_question_by_id, name='get_question_by_id'),
-    # path('api/questions', views.api_questions, name='api_questions'),
-    # path('api/question/<int:question_id>', views.api_question_id, name='api_question_id'),
 ]
--- QCS/Question/views.py
+++ QCS/Question/views.py
@@ -9,31 +9,33 @@ def home(request):
     return TemplateView.as_view(template_name='home.html')
 
 def detail(request):
-    # if request.
+    # Get Latest Ten Questions
+    if request.method == "GET":
+            if request.method == "GET":
+        question_list = Question.objects.order_by('create_date')[:10]
+        context = {
+            'question_list': question_list,
+        }
+        return render(request, 'question.html', context)
 
+    elif request.method == "PUT":
+        # TODO Create
+        if request.user.is_authenticated():
+            return render(request, 'create_question.html')
 
-def question_index(request):
-    question_list = Question.objects.order_by('create_date')[:5]
-    context = {
-        'question_list': question_list,
-    }
-    return render(request, 'question.html', context)
+    
+def detail_Id(request, question_id):
 
-
-def get_question_by_id(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    response = "You're looking at question %s."
-    return HttpResponse(response % question)
-
-
-def api_questions(request):
-    return HttpResponse(serializers.serialize('json', list(Question.objects.all())))
-
-
-def api_question_id(request, question_id):
-    return HttpResponse(serializers.serialize('json', [Question.objects.get(pk=question_id)]))
-
-
-def create(request):
     if request.user.is_authenticated():
-        return render(request, 'create_question.html')
+        if request.method == "GET":
+            question = get_object_or_404(Question, pk=question_id)
+            return HttpResponse(response % question)
+        elif request.method == "POST":
+            # TODO Update
+            if request.user.is_authenticated():
+                question = "ha"
+            return HttpResponse("TBD")
+
+        elif request.method == "DELETE":
+            # TODO Delete
+            return HttpResponse("TBD")
